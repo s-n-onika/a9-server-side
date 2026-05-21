7f2a6c8573583adcf422bd62ac89336fb9ab59d8
S-N-Onika <sumsunnaharonika12@gmail.com>
2026-05-21 19:32:53 +0600

    Config/db file created

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+module.exports = client;
